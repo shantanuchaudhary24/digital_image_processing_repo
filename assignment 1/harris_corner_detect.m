9b49649e9ce243e820891659fe58071dc56f1384
Shantanu Chaudhary <shantanuchaudhary24@gmail.com>
2014-08-23 21:42:29 +0530

    submission code for assignment 1 included and some sample images

--- assignment 1/harris_corner_detect.m
+++ assignment 1/harris_corner_detect.m
@@ -1,72 +1,114 @@
 % harrisDetect Corner Detection to determine corner points of interests.
-% srcImg    :   Image to be processed
-% thresh    :   Threshold for determining points of interests
-% sigma     :   Parameters to vary smoothening
-% outImg    :   Output Image vector
-% xArea     :   length of the area to be considered
-% yArea     :   breadth of the are to be considered
-%
+% inpImg        :   Image to be processed
+% thresh        :   Threshold for determining points of interests
+% sigma         :   Parameters to vary smoothening
+% windowWidth   :   Size of window to analyse R (scoreMatrix)
+% w             :   For filtering
+% k             :   For harris corner measure (Value is between 0.04 -0.06)
 % Shantanu Chaudhary, Indian Institute of Technology, Delhi, August 2014.
 % shantanuchaudhary24@gmail.com, cs5100295@cse.iitd.ac.in
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Harris corner detection function with bilateral filter:
-function [outImg] = harris_corner_detect(srcImg, thresh, sigma ,xArea, yArea)
+function [] = harris_corner_detect(inpImg, sigma , w, k,windowWidth, thresh)
 
 % Verifying number of arguments to the function
-narginchk(5,5);
+% narginchk(7,7); 
+ 
+% Reduce noise in the image by using bilateral filter
 
-% Converting the input source image grayscale values to double if not double already. 
-if ~isa(srcImg,'double')
-    srcImg = double(srcImg);
+% srcImg= bfilter2(inpImg, w, sigma1);
+ if ~isa(inpImg,'double')
+	srcImg = double(inpImg)/255;
+ else
+    srcImg = inpImg/255;
+ end
+[m,n,colorWidth] = size(inpImg);
+
+% Calculation of partial derivatives of I with respect to X and Y uses
+% sobel operators. We define the following Sobel operators as masks
+Gx = [-1 0 1 ; -1 0 1 ; -1  0  1 ];
+Gy = Gx';
+
+% The partial derivatives (approximated) can be obtained by using the
+% convolution of Gx and Gy with srcImg for partial derivatives with respect
+% to X and Y respectively.
+if colorWidth > 1   % For colored picture
+    Ix = conv2(rgb2gray(srcImg), Gx, 'same');
+    Iy = conv2(rgb2gray(srcImg), Gy, 'same');
+    disp('RGB Deteted!');
+else                % For Grayscale Picture
+    Ix = conv2(srcImg, Gx, 'same');
+    Iy = conv2(srcImg, Gy, 'same');
+    disp('Grayscale Detected!');
 end
 
-%Size of image
-[numCols, numRows] = size(srcImg);
+% For Debugging
+% dlmwrite('harris1_Ix', Ix);
+% dlmwrite('harris1_Iy', Iy);
 
+% To calculate Ix^2 and Iy^2 respectively. Point to Point multiplication
+% and not matrix multiplication and smoothened with bilateral filter
+Ix2 = bfilter2(double(Ix.^2)/255, w, sigma);
+Iy2 = bfilter2(double(Iy.^2)/255, w, sigma);
+Ixy = bfilter2(abs(Ix.*Iy)/255, w, sigma);
+
+% Calculation of the harris measure score is done here:
+% R = det(M) - k(trace(M))^2 : where M is the structure tensor calculated
+% det(M) = (Ix2.*Iy2 - Ixy.^2)
+% trace(M) = (Ix + Iy)
+
+  scoreMatrix = (Ix2.*Iy2 - Ixy.^2) - k*(Ix + Iy).^2 ;
+
+% Suppression of minimas and elevation of maximas takes place here:
+  resultScoreMatrix = findCorner(scoreMatrix, windowWidth, thresh);
+
+  [r,c] = find(resultScoreMatrix);
+  figure, imagesc(inpImg), axis image, colormap(gray), hold on
+  plot(c,r,'gs'), title('corners detected');
+return;
+
+% Following code is incase we don't want to use conv function of matlab.
+%{
 % Derivative width
 hx = 1/numCols;
 hy = 1/numRows;
 
-% Here we define the window WILL BE CHANGED NOW
-window = xArea*yArea ;
-
 %Calculation of partial derivatives of I with respect to X and Y (finite distance method)
-Ix = zeros([numCols, numRows]);
-Iy = zeros([numCols, numRows]);
+IxPrime = zeros([numCols, numRows]);
+IyPrime = zeros([numCols, numRows]);
 
 for i=1:numCols-1
     for j=1:numRows
-        Ix(i, j)=(srcImg(i+1, j)-srcImg(i, j))/hx;
+        IxPrime(i, j)=(srcImg(i+1, j)-srcImg(i, j))/hx;
     end
 end
 
 for i=1:numCols
    for j=1:numRows-1
-       Iy(i, j)=(srcImg(i, j+1)-srcImg(i, j))/hy;
+       IyPrime(i, j)=(srcImg(i, j+1)-srcImg(i, j))/hy;
    end
 end
 
-% To calculate Ix^2 and Iy^2 respectively at every pixel.
-Ix2 = zeros([numCols, numRows]);
-Iy2 = zeros([numCols, numRows]);
+% To calculate IxPrime^2 and IyPrime^2 respectively at every pixel.
+IxPrime2 = zeros(numCols, numRows);
+IyPrime2 = zeros(numCols, numRows);
 
 for i=1:numCols
    for j=1:numRows
-       Iy2(i, j)= Iy(i, j)*Iy(i, j);
-       Ix2(i, j)= Ix(i, j)*Ix(i, j);
+       IyPrime2(i, j)= IyPrime(i, j)*IyPrime(i, j);
+       IxPrime2(i, j)= IxPrime(i, j)*IxPrime(i, j);
    end
 end
 
-% To calculate Ixy at every pixel
-Ixy = zeros([numCols, numRows]);
+% To calculate IxyPrime at every pixel
+IxyPrime = zeros([numCols, numRows]);
 for i=1:numCols
    for j=1:numRows
-       Ixy(i, j)= Ix(i, j)*Iy(i, j);
+       IxyPrime(i, j)= IxPrime(i, j)*IyPrime(i, j);
    end
 end
 
-%
-
-
+%}
 
