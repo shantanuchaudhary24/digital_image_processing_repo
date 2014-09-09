087a1a92ae5cbd812b09baa31a4ce0eeed2172ba
Shantanu Chaudhary <shantanuchaudhary24@gmail.com>
2014-09-09 22:57:39 +0530

    Code for assignment 1 as well as assignment 2 main run file

---
+++ assignment 2/main.m
@@ -0,0 +1,16 @@
+
+% Main run script for running the program
+
+% Shantanu Chaudhary, Indian Institute of Technology, Delhi, August 2014.
+% shantanuchaudhary24@gmail.com, cs5100295@cse.iitd.ac.in
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Load test images.
+% Note: Must be double precision in the interval [0,1].
+img1 = imread('pyramid.png');
+img2 = imread('chair.png');
+img3 = imread('einstein.jpg');
+
