e3bc7334115b6de5acc5c1d445750c656feb1cbb
Shantanu Chaudhary <shantanuchaudhary24@gmail.com>
2014-10-14 00:19:41 +0530

    Removed reference image registration code

--- assignment 3/main.m
+++ assignment 3/main.m
@@ -9,27 +9,8 @@
 
 
 % Load test images.
-% Note: Must be double precision in the interval [0,1].
-img1 = imread('images/pyramid.png');
-img2 = imread('images/chair.png');
-img3 = imread('images/einstein.jpg');
+img1 = imread('images/lena.bmp');
+img2 = imread('images/lena_cropped_rotated_shifted.bmp');
 
-% Set bilateral filter parameters.
-w     = 5;       % bilateral filter half-width
-sigma1 = [1.25 0.1]; % bilateral filter standard deviations
-
-%Set Harris parameters
-% Manipulate the following parameters to fine tune corner detection
-k   =   0.04;
-windowWidth = 5;
-threshold = 30;
-
-harris_corner_detect(img1,sigma1, w, k, windowWidth, threshold);
-
-%Set Harris parameters
-% Manipulate the following parameters to fine tune corner detection
-k   =   0.04;
-windowWidth = 7;
-threshold = 0.006;
-
-harris_corner_detect(img2,sigma1, w, k, windowWidth, threshold);
+% Call to Image Registration Function
+image_register(img1, img2);
